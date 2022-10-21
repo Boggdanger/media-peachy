8b78bdb471848af18f6a5c339139f5e27f1b3e94
Bohdan Hoiko <litvipenko@gmail.com>
2022-10-21 21:33:32 +0300

    edits

--- js/gsap-settings.js
+++ js/gsap-settings.js
@@ -3,23 +3,23 @@ gsap.registerPlugin(ScrollTrigger);
 // run line
 gsap.to(".run-line__item-inner_one", {
     scrollTrigger: {
-        trigger: ".header",
+        trigger: ".run-line",
         scrub: 1.4,
         start: "-70vh bottom",
-        markers: true,
+        // markers: true,
     },
-    x:-360,
+    x:-260,
     ease: "none",
     
 });
 
 gsap.to(".run-line__item-inner_two", {
     scrollTrigger: {
-        trigger: ".header",
+        trigger: ".run-line",
         scrub: 1.4,
         start: "-70vh bottom",
     },
-    x:320,
+    x:220,
     ease: "none",
 });
 
