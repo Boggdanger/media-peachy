76ea33397610d2469f4021d1a4da38926ed76165
Bohdan Hoiko <litvipenko@gmail.com>
2022-10-21 21:26:42 +0300

    initial

--- js/gsap-settings.js
+++ js/gsap-settings.js
@@ -3,47 +3,47 @@ gsap.registerPlugin(ScrollTrigger);
 // run line
 gsap.to(".run-line__item-inner_one", {
     scrollTrigger: {
-        trigger: ".run-line",
+        trigger: ".header",
         scrub: 1.4,
         start: "-70vh bottom",
-        // markers: true,
+        markers: true,
     },
-    x:-260,
+    x:-360,
     ease: "none",
     
 });
 
 gsap.to(".run-line__item-inner_two", {
     scrollTrigger: {
-        trigger: ".run-line",
+        trigger: ".header",
         scrub: 1.4,
         start: "-70vh bottom",
     },
-    x:220,
+    x:320,
     ease: "none",
 });
 
-scrTxt.add("(max-width: 768px)", () => {
-    // scroll
-    gsap.to(".run-line__item-inner_one", {
-        scrollTrigger: {
-            trigger: ".run-line",
-            scrub: 1.4,
-            start: "top bottom",
-            // markers: true,
-        },
-        x:-260,
-        ease: "none",
+// scrTxt.add("(max-width: 768px)", () => {
+//     // scroll
+//     gsap.to(".run-line__item-inner_one", {
+//         scrollTrigger: {
+//             trigger: ".run-line",
+//             scrub: 1.4,
+//             start: "top bottom",
+//             // markers: true,
+//         },
+//         x:-260,
+//         ease: "none",
         
-    });
+//     });
 
-    gsap.to(".run-line__item-inner_two", {
-        scrollTrigger: {
-            trigger: ".run-line",
-            scrub: 1.4,
-            start: "top bottom",
-        },
-        x:250,
-        ease: "none",
-    });
-});
+//     gsap.to(".run-line__item-inner_two", {
+//         scrollTrigger: {
+//             trigger: ".run-line",
+//             scrub: 1.4,
+//             start: "top bottom",
+//         },
+//         x:250,
+//         ease: "none",
+//     });
+// });
